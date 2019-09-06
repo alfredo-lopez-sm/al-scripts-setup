97b11d109c14626ca226f8270c362dcfee8c9ef1
alopezsearch <a.lopez@searchmetrics.com>
2019-09-06 17:22:45 +0200

    first commit

---
+++ setup.sh
@@ -0,0 +1,25 @@
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew update
+
+# Install java
+brew cask install java11
+
+# Install git
+brew install git
+git config --global user.name "Alfredo Lopez"
+git config --global user.email "a.lopez@searchmetrics.com"
+
+
+# Install build tools
+brew install maven
+brew install gradle
+
+
+# Install ides
+brew cask install intellij-idea
+brew cask install visual-studio-code
+
+# Install docker
+brew cask install docker
