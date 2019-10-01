ef6b88fd8e0418ea82a8630a72e319e484b7b5c2
Alfredo Lopez <a.lopez@searchmetrics.com>
2019-10-01 13:59:48 +0200

    Add dependency for the intellij plugin for symfony

--- setup.sh
+++ setup.sh
@@ -77,6 +77,7 @@ install_idea_plugin "composer-json-plugin-proguard" "https://plugins.jetbrains.c
 install_idea_plugin "phpstorm-docker" "https://plugins.jetbrains.com/files/8595/69467/phpstorm-docker.zip"
 install_idea_plugin "PhpInspectionsEA" "https://plugins.jetbrains.com/files/7622/69570/PhpInspectionsEA.jar"
 install_idea_plugin "phpstorm-remote-interpreter" "https://plugins.jetbrains.com/files/7511/69468/phpstorm-remote-interpreter.zip"
+install_idea_plugin "twig" "https://plugins.jetbrains.com/files/7303/69469/twig.zip"
 install_idea_plugin "symfony-plugin" "https://plugins.jetbrains.com/files/7219/69177/symfony-plugin-0.19.184.zip"
 
 
